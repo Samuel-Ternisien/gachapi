ca94f058ab1833812bbd5ada0ea57cd356763724
samuel <samuel.ternisien@gmail.com>
2024-02-15 13:45:20 +0100

    Initial commit

---
+++ gachapi/src/main/java/imt/api/gachapi/GachapiApplication.java
@@ -0,0 +1,13 @@
+package imt.api.gachapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GachapiApplication.class, args);
+	}
+
+}
---
+++ gachapi/src/test/java/imt/api/gachapi/GachapiApplicationTests.java
@@ -0,0 +1,13 @@
+package imt.api.gachapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
